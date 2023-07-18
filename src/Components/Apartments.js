8392128e8156958e9c9c143c4d7cb02476328c49
Cyriacus Elochukwu Daniel <cyriacuselochukwu1@gmail.com>
2023-07-18 12:29:20 +0300

    Making the applestore and playstore imgs buttons

--- src/Components/Apartments.js
+++ src/Components/Apartments.js
@@ -3,29 +3,32 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-import Product from './Product';
-import { productData } from './data';
+import Product from "./Product";
+import { productData } from "./data";
 
 function Apartments() {
-
   const products = productData.map((product, i) => (
-    <SwiperSlide key={i}><Product
-          key={product.id}
-          name={product.name}
-          url={product.imageurl}
-          location={product.location}
-    /></SwiperSlide>
+    <SwiperSlide key={i}>
+      <Product
+        key={product.id}
+        name={product.name}
+        url={product.imageurl}
+        location={product.location}
+      />
+    </SwiperSlide>
   ));
 
   return (
-    <div className="mt-5">
+    <div className="mt-7">
       <div className="flex items-center flex-col justify-center">
         <h1 className="font-sfprobold md:text-2xl text-base text-[rgb(38,38,38)] flex-row">
           APARTMENTS AND SITES
         </h1>
-        <div className="md:mx-6 mx-3 flex-grow">
+        <div className="md:mx-6 mx-3 mt-3 flex-grow">
           <p className="font-sfproregular text-xs md:text-sm mt-6 text-[#5A5A5A] flex-row">
-            Resident takes care of all aspects of property management, including tenants screening, lease agreements, rent collection, and handling maintenance and repair requests.
+            Resident takes care of all aspects of property management, including
+            tenants screening, lease agreements, rent collection, and handling
+            maintenance and repair requests.
           </p>
         </div>
       </div>
@@ -49,14 +52,11 @@ function Apartments() {
           }}
           loop={true}
           modules={[Navigation]}
-          
-      
         >
           {products}
-       
         </Swiper>
       </div>
     </div>
   );
 }
-export default Apartments;                  
+export default Apartments;
--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,29 +1,49 @@
-import React from 'react'
-import appstore from "../Assets/Icons/AppStore.png"
-import playstore from "../Assets/Icons/GooglePlay.png"
+import React from "react";
+import appstore from "../Assets/Icons/AppStore.png";
+import playstore from "../Assets/Icons/GooglePlay.png";
 
-import homescreen from '../Assets/Images/Rmobileapp.png'
-import payscreen from '../Assets/Images/Rpayscreen.png'
+import homescreen from "../Assets/Images/Rmobileapp.png";
+import payscreen from "../Assets/Images/Rpayscreen.png";
 
 function HeroSection() {
   return (
-    <div className='itms-center justify-center text-center mt-16'>
-      <h1 className='font-sfprobold md:text-2xl text-base text-[#262626]'>UNLOCKING SEAMLESS PROPERTY</h1>
-      <h1 className='font-sfprobold md:text-2xl text-base text-[#262626]'>MANAGEMENT SOLUTIONS</h1>
-      <div className='md:mx-60 mx-4'>
-      <p className='font-sfproregular text-xs scale-95 md:text-sm mt-6 text-[#5A5A5A]'>Experience hassle-free property management like never before with Residence. Our comprehensive suite of services bridges the gap between tenants and landlords, offering a seamless experience for all.</p>
+    <div className="itms-center justify-center text-center mt-16">
+      <h1 className="font-sfprobold md:text-2xl text-base text-[#262626]">
+        UNLOCKING SEAMLESS PROPERTY
+      </h1>
+      <h1 className="font-sfprobold md:text-2xl text-base text-[#262626]">
+        MANAGEMENT SOLUTIONS
+      </h1>
+      <div className="md:mx-60 mx-4">
+        <p className="font-sfproregular text-xs scale-95 md:text-sm mt-6 text-[#5A5A5A]">
+          Experience hassle-free property management like never before with
+          Residence. Our comprehensive suite of services bridges the gap between
+          tenants and landlords, offering a seamless experience for all.
+        </p>
       </div>
-      <div className='flex items-center mt-10 md:space-x-8 space-x-4 justify-center'>
-       <img src={appstore} alt='greem' className='h-10 cursor-pointer'/>
-       <img src={playstore} alt='greem' className='h-10 cursor-pointer'/>
+      <div className="flex items-center mt-10 md:space-x-8 space-x-4 justify-center">
+        <a
+          href="https://apps.apple.com/ng/app/residents/id1669505527"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={appstore} alt="greem" className="h-10 cursor-pointer" />
+        </a>
+        <a
+          href="https://play.google.com/store/apps/details?id=com.syrysdevelopers.syrysresidents"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={playstore} alt="greem" className="h-10 cursor-pointer" />
+        </a>
       </div>
 
-      <div className='flex items-center mt-20 space-x-8 justify-center'>
-      <img src={homescreen} alt='greem' className='md:h-72 h-40'/>
-      <img src={payscreen} alt='greem' className='md:h-72 h-40'/>
-     </div>
+      <div className="flex items-center mt-20 space-x-8 justify-center">
+        <img src={homescreen} alt="greem" className="md:h-72 h-40" />
+        <img src={payscreen} alt="greem" className="md:h-72 h-40" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
